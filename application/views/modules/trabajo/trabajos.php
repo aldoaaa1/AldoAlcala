a40fa33d434b6566afbc81b3f357a2e3e044e6c2
Aldo Alcalá <aldo.aaa1@gmail.com>
2019-03-20 22:02:21 -0600

    Corrección de redacción (Mamá)
    - Redacción
    - Responsive para 1920px

--- application/views/modules/trabajo/trabajos.php
+++ application/views/modules/trabajo/trabajos.php
@@ -3,54 +3,62 @@
 		<div>
 			<h3 class="titulo1 text-light">TRABAJOS QUE OFREZCO</h3>
 			<div class="row">
-				<div class="col-lg-3">
+				<div class="col-lg-4">
 					<h4 class="text-light text-center">Sonorización</h4>
-					<p class="text-light trabajos_p">&nbsp&nbsp
-						¡Consigue transmitir lo que necesitas! No permitas que tanto esfuerzo, al final termine en una mala regulacion/calibración de audio y que la gente no pueda recibir lo que tu tenias para ellos en ese momento especial. Dame la oportunidad y yo regulo tu evento.
+					<p class="text-light trabajos_p text-justify">&nbsp&nbsp
+						¡Consigue transmitir lo que necesitas! No permitas que tanto esfuerzo termine en una mala regulacion/calibración de audio porque la gente no podrá recibir lo que ofreces. Dame la oportunidad para regular tu evento.
 					</p>
 				</div>
-				<div class="col-lg-3">
+				<div class="col-lg-4">
 					<h4 class="text-light text-center">Arreglos acústicos</h4>
-					<p class="text-light trabajos_p">&nbsp&nbsp
-						Nada peor que la gente al final no alcance a entender el mensaje. Si en el lugar en donde tu estas no puedes entender nada de lo que dicen o los intrumentos que tocan, lo mas probable es que tengas un problema acústico. Déjame ir a hacer algunas pruebas, y te digo como podemos solucionarlo.
+					<p class="text-light trabajos_p text-justify">&nbsp&nbsp
+						Si la acústica del lugar impide entender el mensaje o disfrutar los instrumentos. Déjame ir a hacer algunas pruebas, y te digo como podemos solucionarlo.
 					</p>
 				</div>
-				<div class="col-lg-3">
+				<div class="col-lg-4">
 					<h4 class="text-light text-center">Clases de música</h4>
-					<p class="text-light trabajos_p">&nbsp&nbsp
-						Si lo que quieres es ir elevando tu el nivel musical. ¡Yo puedo ayudarte con eso! Doy clases de piano, canto, batería, guitarra y bajo. Mándame un mensaje y nos ponemos de acuerdo.
+					<p class="text-light trabajos_p text-justify">&nbsp&nbsp
+						Si lo que quieres es ir elevando tu nivel musical. ¡Permíteme apoyarte! Doy clases de piano, canto, batería, guitarra y bajo. ¡Pongámonos de acuerdo!
 					</p>
 				</div>
-				<div class="col-lg-3">
-					<h4 class="text-light text-center">Creación de secuencias</h4>
-					<p class="text-light trabajos_p">&nbsp&nbsp
-						Si ya no tienes suficientes elementos para tocar, eso ya no es problema, yo puedo grabar esos instrumentos que te faltan para que puedas tocar siempre completo y sin miedo a que alguien te falte en alguna presentación. ¡Siempre te escucharas bien!
-					</p>
-				</div>			
 			</div>
 			<div class="row">
-				<div class="col-lg-3">
+				<div class="col-lg-4">
+					<h4 class="text-light text-center">Creación de secuencias</h4>
+					<p class="text-light trabajos_p text-justify">&nbsp&nbsp
+						¿Quieres enriquecer tus presentaciones? Puedo grabar esos instrumentos extras para que toques siempre completo y sin miedo a que alguien te falte. ¡Siempre te escucharás bien!
+					</p>
+				</div>			
+				<div class="col-lg-4">
 					<h4 class="text-light text-center">Manejo de secuencias</h4>
-					<p class="text-light trabajos_p">&nbsp&nbsp
-						Si eso de las secuencias es un tema nuevo para ti, yo te puedo capacitar en el tema y ayudarte a realizar la instalación necesaria para que puedas empezar a utilizarlas en donde estás. ¡Nada como una buena asesoría para emprender algo nuevo!
+					<p class="text-light trabajos_p text-justify">&nbsp&nbsp
+						Si eso de las secuencias es un tema nuevo para ti, brindo capacitación en el tema y realizo la instalación. ¡Nada como una buena asesoría para emprender algo nuevo!
 					</p>
 				</div>
-				<div class="col-lg-3">
+				<div class="col-lg-4">
 					<h4 class="text-light text-center">Ensambles</h4>
-					<p class="text-light trabajos_p">&nbsp&nbsp
-						Si en tu banda sientes tienes buenos elementos, pero aun asi, no sabes porque no suena lo suficientemente "amarradita", o has escuchado muchas veces eso de "te falta base" entonces es un problema de ensamble. ¡Déjame asesorar a tu banda y ayudarte con eso!
+					<p class="text-light trabajos_p text-justify">&nbsp&nbsp
+						Tienes buenos elementos, pero aún asi no suena "amarradito", o dicen que "te falta base"; entonces, es un problema de ensamble. ¡Déjame asesorar a tu banda!
 					</p>
 				</div>
-				<div class="col-lg-3">
+			</div>
+			<div class="row">
+				<div class="col-lg-4">
 					<h4 class="text-light text-center">Instalación de equipo</h4>
-					<p class="text-light trabajos_p">
-						Elige el mejor equipo en base a las necesidades de tu recinto, y asegura una instalación limpia y sin errores para que todo funcione como debe de ser y tengas un equipo escalable para que puedas irlo complementando conforme vayas creciendo.
+					<p class="text-light trabajos_p text-justify">
+						Eligamos el mejor equipo con base a las características de tu recinto, y asegura un óptimo funcionamiento, de tal manera que puedas complementarlo conforme vayas creciendo.
 					</p>
 				</div>
-				<div class="col-lg-3">
+				<div class="col-lg-4">
 					<h4 class="text-light text-center">Calibración de equipo</h4>
-					<p class="text-light trabajos_p">
-						Si tu sabes que tienes buen equipo, pero aun asi no sabes porque no suena como quieres, entonces lo mas probable es que tengas un problema de calibración inicial. Necesitamos ir a realizar unas pruebas y calibrar tu equipo para que puedas exprimirlo al máximo.
+					<p class="text-light trabajos_p text-justify">
+						Si tú sabes que tienes buen equipo, pero aún así no sabes por qué no suena como quieres; hagámos pruebas y calibremos tu equipo para que puedas exprimirlo al máximo.
+					</p>
+				</div>
+				<div class="col-lg-4">
+					<h4 class="text-light text-center">Músicos y coros</h4>
+					<p class="text-light trabajos_p text-justify">&nbsp&nbsp
+						Tanto para tus grabaciones como para tus eventos en vivo, puedo conseguirte a las personas adecuadas para completar tu banda.
 					</p>
 				</div>
 			</div>
