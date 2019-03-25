5b019ac9cb187234907262795592195d2e927252
Aldo Alcalá <aldo.aaa1@gmail.com>
2019-03-24 22:52:29 -0600

    Optimización en carga de página

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,2 +1,10 @@
 // LA VELOCIDAD DE LA ANIMACION DEL SCROLL DE LOS MENÚS
 var velocidadScroll = 700;
+
+function showLoader(){
+	Swal.fire({
+      html: '<div class="loader"><div></div><div></div><div></div><div></div></div>', showConfirmButton:false,
+      allowEscapeKey:false, allowEnterKey:false, allowOutsideClick:false, keydownListenerCapture: true
+    })
+}
+function closeLoader(){ Swal.close(); }
