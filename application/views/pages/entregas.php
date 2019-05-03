9b858713f276b2a2a8e87b77cdbd60351550517b
Aldo Alcalá <aldo.aaa1@gmail.com>
2019-05-03 18:25:00 -0500

    Pagina de Entragas
    Inicio

---
+++ application/views/pages/entregas.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="<?php echo (base_url().'assets/css/entregas.css') ?>">
+
+
+
+<script type="text/javascript"src="<?php echo (base_url().'assets/js/entregas.js') ?>" async></script>
