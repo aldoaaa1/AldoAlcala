825da98fc9128391966ce53d7913615f28c29eb0
Aldo Alcalá <aldo.aaa1@gmail.com>
2019-05-04 10:23:22 -0500

    Correción en Envío de Estadísticas
    - Se corrige la página actual.

--- assets/js/registrar_visita.php
+++ assets/js/registrar_visita.php
@@ -9,7 +9,7 @@ user.language = a.language;
 user.vendor = a.vendor; 
 user.userAgent = a.userAgent; 
 user.ip = "<?= $_SERVER['REMOTE_ADDR'] ?>"; 
-user.paginaActual = document.referrer; 
+user.paginaActual = document.URL; 
  
 // REGISTRO EN BASE DE DATOS 
 $.post(baseUrl+'reg', {'user': user});
